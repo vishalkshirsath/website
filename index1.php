7d6068867be99d62e7f2700f99937c05d39eba7a
Vishal Kailas Shirsath <135831967+vishalkshirsath@users.noreply.github.com>
2023-12-17 02:55:28 +0530

    Update index1.php

--- index1.php
+++ index1.php
@@ -1,8 +1,27 @@
+<?php
+// Database configuration
+$servername = "vishal.mysql.database.azure.com"; // e.g., "localhost"
+$username = "vishal";
+$password = "Vigo@4242";
+$dbname = "info";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+// If the connection is successful, you can perform database operations here
+
+// Close the connection when done
+$conn->close();
+?>
 <!DOCTYPE html>
 <html>
   <head>
     <title>Vishal Shirsath</title>
-    <meta name="keywords" content="vishal shirsath, Software Developer, Ethical Hacker.">
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="description" content="Vishal Shirsath software developer, ethical hacker ">
@@ -49,185 +68,4 @@ necessary skills as well as be motivated by the company to do his best for the s
 himself and the company advancement in the software engineering industry.
     </p>
     
-    <h2 class="w3-padding-16 w3-text-light-white">My Skills</h2>
-    <p class="w3-wide ">Web Development</p>
-    <div class="w3-white">
-      <div class="w3-light-green" style="height:28px;width:95%"></div>
-    </div>
-    <p class="w3-wide">programming</p>
-    <div class="w3-white">
-      <div class="w3-light-green" style="height:28px;width:85%"></div>
-    </div>
-    <p class="w3-wide">Cyber Security</p>
-    <div class="w3-white">
-      <div class="w3-light-green" style="height:28px;width:80%"></div>
-    </div><br>
-      <div class="w3-row w3-center w3-padding-16 w3-section w3-light-grey">
-      <div class="w3-quarter w3-section">
-        <span class="w3-xlarge">11+</span><br>
-        Partners
-      </div>
-      <div class="w3-quarter w3-section">
-        <span class="w3-xlarge">55+</span><br>
-        Projects Done
-      </div>
-      <div class="w3-quarter w3-section">
-        <span class="w3-xlarge">89+</span><br>
-        Happy Clients
-      </div>
-      <div class="w3-quarter w3-section">
-        <span class="w3-xlarge">150+</span><br>
-        Meetings
-      </div>
-    </div>
-
-    <button class="w3-button w3-light-grey w3-padding-large w3-section">
-      <i class="fa fa-download"></i> Download Resume
-    </button>
     
-    <!-- Grid for pricing tables -->
-    <h2 class="w3-padding-16 w3-text-light-grey">My Price</h2>
-    <div class="w3-row-padding" style="margin:0 -16px">
-      <div class="w3-half w3-margin-bottom">
-        <ul class="w3-ul w3-white w3-center w3-opacity w3-hover-opacity-off">
-          <li class="w3-light-green w3-xlarge w3-padding-32">Basic</li>
-          <li class="w3-padding-16">Web Development</li>
-          <li class="w3-padding-16">HTML,CSS,JS</li>
-          <li class="w3-padding-16">5GB Storage</li>
-          <li class="w3-padding-16">Mail Support</li>
-          <li class="w3-padding-16">
-            <h2>$ 10</h2>
-            <span class="w3-opacity">per month</span>
-          </li>
-          <li class="w3-light-grey w3-padding-24">
-            <button class="w3-button w3-white w3-padding-large w3-hover-black">Sign Up</button>
-          </li>
-        </ul>
-      </div>
-
-      <div class="w3-half">
-        <ul class="w3-ul w3-white w3-center w3-opacity w3-hover-opacity-off">
-          <li class="w3-teal w3-xlarge w3-padding-32">Pro</li>
-          <li class="w3-padding-16">Web Development</li>
-          <li class="w3-padding-16">HTML,CSS,JS,PHP,SQL,PYTHON</li>
-          <li class="w3-padding-16">50GB Storage</li>
-          <li class="w3-padding-16">Endless Support</li>
-          <li class="w3-padding-16">
-            <h2>$ 25</h2>
-            <span class="w3-opacity">per month</span>
-          </li>
-          <li class="w3-light-grey w3-padding-24">
-            <button class="w3-button w3-white w3-padding-large w3-hover-black">Sign Up</button>
-          </li>
-        </ul>
-      </div>
-    <!-- End Grid/Pricing tables -->
-    </div>
-  <?php
-$con = mysqli_init();
-mysqli_ssl_set($con, NULL, NULL, "{path to CA cert}", NULL, NULL);
-mysqli_real_connect($con, "vishalshirsath-server.mysql.database.azure.com", "byjmdcqsuo", "Vigo@4242", "info", 3306, MYSQLI_CLIENT_SSL);
-
-// Check connection
-if (!$con) {
-    die("Connection failed: " . mysqli_connect_error());
-}
-
-echo "Connected successfully";
-
-// Perform your database operations here
-
-// Query to fetch work experience data
-$workExperienceQuery = "SELECT * FROM work_experience";
-$workExperienceResult = mysqli_query($con, $workExperienceQuery);
-
-// Display work experience data
-if ($workExperienceResult && mysqli_num_rows($workExperienceResult) > 0) {
-    while ($row = mysqli_fetch_assoc($workExperienceResult)) {
-        echo "<div class='w3-container'>";
-        echo "<h4 class='w3-text-white'><b>" . $row['job_title'] . "</b></h4>";
-        echo '<h6 class="w3-text-teal"><i class="fa fa-calendar fa-fw w3-margin-right"></i>' . $row['duration'] . '</h6>';
-        echo '<p>' . $row['description'] . '<br>Job Nature – ' . $row['job_nature'] . '<br></p>';
-        echo '<hr>';
-        echo '</div>';
-    }
-} else {
-    echo '<p>No work experience data available.</p>';
-}
-
-// Close the work experience query
-mysqli_free_result($workExperienceResult);
-
-// Education Section
-// Query to fetch education data
-$educationQuery = "SELECT * FROM education";
-$educationResult = mysqli_query($con, $educationQuery);
-
-// Display education data
-if ($educationResult && mysqli_num_rows($educationResult) > 0) {
-    while ($row = mysqli_fetch_assoc($educationResult)) {
-        echo '<div class="w3-container">';
-        echo '<h4 class="w3-opacity"><b>' . $row['institution'] . '</b></h4>';
-        echo '<h5 class="w3-text-teal"><i class="fa fa-calendar fa-fw w3-margin-right"></i>' . $row['duration'] . '</h5>';
-        echo '<h6>' . $row['degree'] . '</h6>';
-        echo '<hr>';
-        echo '</div>';
-    }
-} else {
-    echo '<p>No education data available.</p>';
-}
-
-// Close the education query
-mysqli_free_result($educationResult);
-
-// Close the connection
-mysqli_close($con);
-?>
-
-
-    </div>
-<div class="w3-padding-64 w3-content w3-text-grey" id="contact">
-  <h2 class="w3-text-light-grey">Contact Me</h2>
-  <hr style="width:200px" class="w3-opacity">
-
-  <div class="w3-section">
-    <p class="w3-text-white"><i class="fa fa-map-marker fa-fw w3-text-teal w3-xxlarge w3-margin-right"></i>Nashik, India</p>
-    <p class="w3-text-white"><i class="fa fa-phone fa-fw w3-text-teal w3-xxlarge w3-margin-right"></i> Phone: +919067339037</p>
-    <p class="w3-text-white"><i class="fa fa-envelope fa-fw w3-text-teal w3-xxlarge w3-margin-right"> </i> Email: shirsathvishal70@gmail.com</p>
-  </div><br>
-  <p class="w3-text-white">Let's get in touch. Send me a message:</p>
-
-  <form action="/action_page.php" target="_blank">
-    <p><input class="w3-input w3-padding-16" type="text" placeholder="Name" required name="Name"></p>
-    <p><input class="w3-input w3-padding-16" type="text" placeholder="Email" required name="Email"></p>
-    <p><input class="w3-input w3-padding-16" type="text" placeholder="Subject" required name="Subject"></p>
-    <p><input class="w3-input w3-padding-16" type="text" placeholder="Message" required name="Message"></p>
-    <p>
-      <button class="w3-button w3-light-grey w3-padding-large" type="submit">
-        <i class="fa fa-paper-plane"></i> SEND MESSAGE
-      </button>
-    </p>
-  </form>
-
-</div>
-
-  
-<!-- Footer. This section contains an ad for W3Schools Spaces. You can leave it to support us. -->
-<footer class="w3-content w3-padding-64 w3-xlarge">
- <h1> <i class="fa fa-facebook-official w3-hover-opacity w3-text-blue"></i>
-  <i class="fa fa-instagram w3-hover-opacity w3-text-pink"></i>
-  <i class="fa fa-snapchat w3-hover-opacity w3-text-yellow"></i>
-  <i class="fa fa-pinterest-p w3-hover-opacity w3-text-yellow"></i>
-  <i class="fa fa-twitter w3-hover-opacity w3-text-blue"></i>
-  <i class="fa fa-linkedin w3-hover-opacity w3-text-indigo"></i></h1>
- <p class="w3-small">Copyright © 2022 - 2023 Vishal Shirsath®. All rights reserved. </p>
- 
-</footer>
-
-<!-- END PAGE CONTENT -->
-</div>
-</div>
-</div>
-</div>
-</body>
-</html>

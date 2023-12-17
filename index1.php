e2e1c40e9a3d8b59948daa208b820f9af4ff0921
Vishal Kailas Shirsath <135831967+vishalkshirsath@users.noreply.github.com>
2023-12-17 14:24:58 +0530

    Update index1.php

--- index1.php
+++ index1.php
@@ -38,7 +38,7 @@ $conn = sqlsrv_connect($serverName, $connectionInfo);
 
     $conn = mysqli_init();
 mysqli_ssl_set($con,NULL,NULL, "DigiCertGlobalRootCA.crt.pem", NULL, NULL);
-mysqli_real_connect($conn, "vishal.mysql.database.azure.com", "vishal", "Vigo@4242", "contact", 3306, MYSQLI_CLIENT_SSL);
+mysqli_real_connect($conn, "vishal.mysql.database.azure.com", "vishal", "Vigo@4242", "info", 3306, MYSQLI_CLIENT_SSL);
     // Create connection
     
     // Check connection

d93a422059d9c4f1a47b6707e2fbd24afa7936e1
Vishal Kailas Shirsath <135831967+vishalkshirsath@users.noreply.github.com>
2023-12-17 14:07:46 +0530

    Update index1.php

--- index1.php
+++ index1.php
@@ -9,19 +9,36 @@
 
 <?php
 // Process form submission
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
+//if ($_SERVER["REQUEST_METHOD"] == "POST") {
     // Replace these values with your actual database credentials
    
-    $conn = mysqli_init();
-mysqli_ssl_set($con,NULL,NULL, "DigiCertGlobalRootCA.crt.pem", NULL, NULL);
-mysqli_real_connect($conn, "vishal.mysql.database.azure.com", "vishal", "Vigo@4242", "info", 3306, MYSQLI_CLIENT_SSL);
+  //  $conn = mysqli_init();
+// mysqli_ssl_set($con,NULL,NULL, "DigiCertGlobalRootCA.crt.pem", NULL, NULL);
+//mysqli_real_connect($conn, "vishal.mysql.database.azure.com", "vishal", "Vigo@4242", "info", 3306, MYSQLI_CLIENT_SSL);
+
+    //<?php
+// PHP Data Objects(PDO) Sample Code:
+try {
+    $conn = new PDO("sqlsrv:server = tcp:serverdatabas.database.windows.net,1433; Database = contact", "vishal", "{your_password_here}");
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}
+catch (PDOException $e) {
+    print("Error connecting to SQL Server.");
+    die(print_r($e));
+}
+
+// SQL Server Extension Sample Code:
+$connectionInfo = array("UID" => "vishal", "pwd" => "Vigo@4242", "Database" => "contact", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
+$serverName = "tcp:serverdatabas.database.windows.net,1433";
+$conn = sqlsrv_connect($serverName, $connectionInfo);
+?>
 
     // Create connection
     
     // Check connection
-    if ($conn->connect_error) {
-        die("Connection failed: " . $conn->connect_error);
-    }
+   // if ($conn->connect_error) {
+     //   die("Connection failed: " . $conn->connect_error);
+    //}
 
     // Get form data
     $name = $_POST["name"];
